41945f031f03d2903fb795efa37685b702af6da4
Axolotle <nchesnais@mailoo.org>
2019-01-21 17:34:33 +0100

    switch to manual update & update

--- parser/parserpad.py
+++ parser/parserpad.py
@@ -6,30 +6,26 @@ import locale
 
 locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
 
-#DIR = '/var/www/1000joursenmars/'
-DIR = '/home/univers-b/dev/1000joursenmars/'
-
-
 def isLineInt(s):
-    try: 
+    try:
         int(s)
         return True
     except ValueError:
         return False
 
 def marsToDate(d):
-    daysToAdd = eval(d) - 31
+    daysToAdd = int(d) - 31
     newDate = date(2016, 3, 31) + timedelta(days=daysToAdd)
     dateToPrint = newDate.strftime("%d %B %Y")
     return dateToPrint
 
 def orthoTypo(text, rules):
-    for i, j in rules.items(): 
+    for i, j in rules.items():
         text = text.replace(i, j)
     return text
 
 
-rules = { " ;": "&nbsp;;", 
+rules = { " ;": "&nbsp;;",
           " :": "&nbsp;:",
           " ?": "&nbsp;?",
           " !": "&nbsp;!",
@@ -43,19 +39,16 @@ rules = { " ;": "&nbsp;;",
 def generateHTML() :
     html = [ ]
 
-    with open(DIR + 'parser/main.html', 'r') as main:
+    with open('main.html', 'r') as main:
         for line in main:
             html.append(line.strip('\n'))
 
 
     html.append('\n<div id="main" class="cols">')
 
-    with urlopen('https://strasbourg.nuitdebout.fr/pad/p/1000joursenmars/export/txt') as pad1000:
+    with open('padContent.txt', 'r') as pad1000:
         first = True
-        for _ in range(24):
-            next(pad1000)
         for line in pad1000:
-            line = line.decode('utf-8')
             day = line.split(' ', 1)[0]
 
             if len(line.split()) > 4:
@@ -80,7 +73,7 @@ def generateHTML() :
 
     html.append('        </div>\n    </div>\n</div>\n')
 
-    with open(DIR + 'parser/footer.html', 'r') as footer:
+    with open('footer.html', 'r') as footer:
         for line in footer:
             html.append(line.strip('\n'))
 
@@ -90,7 +83,7 @@ def main():
     html = generateHTML()
     html = '\n'.join(html)
 
-    with open(DIR + "index.html","w") as output :
+    with open("../index.html","w") as output :
         output.write(html)
 
 
